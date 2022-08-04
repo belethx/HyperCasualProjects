1d460d43865e1714d8af50d0aeb67ddb075d7a46
PitoYaga <efe.canoglu@hotmail.com>
2022-08-05 00:23:00 +0300

    upgrade system organized

--- Bowling/Assets/Scripts/PlayerUpgrades.cs
+++ Bowling/Assets/Scripts/PlayerUpgrades.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 
 public class PlayerUpgrades : MonoBehaviour
 {
+    [SerializeField] float _smootnessIndex = 0;
+    [SerializeField] private float varnisCounter= 0.40f;
     [SerializeField] private Material[] _varnisMaterials;
     [SerializeField] private Material[] _emeryMaterials;
     
     private Material _currentMaterial;
-    private float varnisCounter;
     private float emeryCounter;
-    
+
+    private void Awake()
+    {
+        _currentMaterial = GetComponent<Renderer>().sharedMaterial;
+    }
+
     void Start()
     {
-        
+        //başlangıç smoothness'ı 0 olacak
     }
 
     void Update()
     {
-        
+        VarnishUpgrade();
     }
 
+    void VarnishUpgrade()
+    {
+        //material'in smmothness'ı değeri katsayıya göre artacak
+    }
+    
+    void EmeryUpgrade()
+    {
+        //material emeryCounter sayısına göre arrey'den değişecek
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(Constants.varnishTag))
         {
-            varnisCounter++;
+            _smootnessIndex += varnisCounter;
         }
         else if (other.CompareTag(Constants.emeryTag))
         {
@@ -36,15 +52,5 @@ public class PlayerUpgrades : MonoBehaviour
         {
             varnisCounter--;
         }
-    }
-
-    void VarnishUpgrade()
-    {
-        
-    }
-    
-    void EmeryUpgrade()
-    {
-        
     }
 }
